ebd1d2a8f301ea4d46721e44943af977710cc5e9
Edison Pineda <edison.pinedan@ug.edu.ec>
2024-05-09 15:08:04 -0500

    environment component main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import { environmentsComponent } from './src/concepts/01-enviroments';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -16,5 +17,9 @@ document.querySelector('#app').innerHTML = `
     </div>
     
   </div>
-`
+`;
+
+const element = document.querySelector('.card');
+
+environmentsComponent(element);
 
