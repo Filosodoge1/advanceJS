4b8a5de7d048989007f2b6decc457404f923aa52
Edison Pineda <edison.pinedan@ug.edu.ec>
2024-05-09 15:15:06 -0500

    Modificaciones en el .env

--- src/concepts/01-enviroments.js
+++ src/concepts/01-enviroments.js
@@ -4,6 +4,9 @@
  * @param {HTMLDivElement} element 
  */
 export const environmentsComponent = ( element ) => {
+    
+    console.log( import.meta.env );
+
     const html = `
         variables:
     `;
