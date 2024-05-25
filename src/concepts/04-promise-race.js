56dde0a95f3d86d970ff6c5464362c7000176696
Edison Pineda <edison.pinedan@ug.edu.ec>
2024-05-25 16:31:41 -0500

    Promise race (slow, medium, fast) setTimeout

--- src/concepts/04-promise-race.js
+++ src/concepts/04-promise-race.js
@@ -7,4 +7,41 @@ export const promiseRaceComponent = ( element ) => {
     
     console.log( 'promise race component' );
 
+    const renderValue = ( value ) => {
+        element.innerHTML = value;
+    }
+
+    Promise.race([
+        slowPromise(),
+        mediumPromise(),
+        fastPromise(),
+        slowPromise(),
+        slowPromise(),
+        mediumPromise()
+    ]).then( resultado => renderValue(resultado) );
+
+    const slowPromise = () => {
+        return new Promise( resolve => {
+            setTimeout(() => {
+                console.log('Slow Promise');
+            }, 2000);
+        });
+    }
+
+    const mediumPromise = () => {
+        return new Promise( resolve => {
+            setTimeout(() => {
+                console.log('Medium Promise');
+            }, 1500);
+        });
+    }
+
+    const fastPromise = () => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                console.log('Fast Promise');
+            }, 1000);
+        });
+    }
+
 }
