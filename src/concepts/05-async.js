ba79c51ef35e72cf569bdb52108db5eff447f1b1
Edison Pineda <edison.pinedan@ug.edu.ec>
2024-05-26 16:22:19 -0500

    Async - Manejo de errores con throw

--- src/concepts/05-async.js
+++ src/concepts/05-async.js
@@ -10,15 +10,23 @@ export const asyncComponent = ( element ) => {
         element.innerHTML = value;
     }
 
+    const renderError = ( error ) => {
+        element.innerHTML = error;
+    }
+
     const id = '5d86371f25a058e5b1c8a65e';
 
     findHero( id )
-        .then( ({name}) => renderComponent(name) );
+        .then( ({name}) => renderComponent(name) )
+        .catch( error => renderError(error));
 
 }
 
 const findHero = async( id ) => {
     const  hero = heroes.find( heros => heros.id === id);
+    if(!hero){
+        throw `id ${ id } not found`;
+    }
     return hero;
 }
 
