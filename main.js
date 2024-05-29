6072e9b0811d0c5eeb66da4b8ead81eb2274d6d3
Edison Pineda <edison.pinedan@ug.edu.ec>
2024-05-29 16:09:40 -0500

    For await - ejemplo

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ import { promiseRaceComponent } from './src/concepts/04-promise-race';
 import { asyncComponent } from './src/concepts/05-async';
 import { asyncAwaitComponent } from './src/concepts/06-async-await';
 import { asynAwait2Component } from './src/concepts/07-async-await-2';
+import { forAwaitComponent } from './src/concepts/08-for-await';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -33,5 +34,6 @@ const element = document.querySelector('.card');
 // promiseRaceComponent(element);
 // asyncComponent(element);
 // asyncAwaitComponent(element);
-asynAwait2Component(element);
+// asynAwait2Component(element);
+forAwaitComponent(element);
 
